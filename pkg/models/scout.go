b5adb04a80bc97869d112c1fe1f1d2955261e105
mark <mark.poling@gmail.com>
2020-12-28 20:39:37 -0500

    putzing

--- pkg/models/scout.go
+++ pkg/models/scout.go
@@ -7,12 +7,12 @@ import (
 //Scout is scout
 type Scout struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	lastname  string             `bson:"lastname,omitempty" json:"lastname,omitempty"`
-	firstname string             `bson:"firstname,omitempty" json:"firstname,omitempty"`
-	rank      string             `bson:"rank,omitempty" json:"rank,omitempty"`
-	den       int32              `bson:"den,omitempty" json:"den,omitempty"`
-	address   string             `bson:"address,omitempty" json:"address,omitempty"`
-	parents   string             `bson:"parents,omitempty" json:"parents,omitempty"`
-	phone     string             `bson:"phone,omitempty" json:"phone,omitempty"`
-	email     string             `bson:"email,omitempty" json:"email,omitempty"`
+	Lastname  string             `bson:"lastname,omitempty" json:"lastname,omitempty"`
+	Firstname string             `bson:"firstname,omitempty" json:"firstname,omitempty"`
+	Rank      string             `bson:"rank,omitempty" json:"rank,omitempty"`
+	Den       int32              `bson:"den,omitempty" json:"den,omitempty"`
+	Address   string             `bson:"address,omitempty" json:"address,omitempty"`
+	Parents   string             `bson:"parents,omitempty" json:"parents,omitempty"`
+	Phone     string             `bson:"phone,omitempty" json:"phone,omitempty"`
+	Email     string             `bson:"email,omitempty" json:"email,omitempty"`
 }
