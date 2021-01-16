303a75e452e090be97d9a570593f3c205b2eb4c8
Mark Poling <mark.poling@gmail.com>
2021-01-16 09:58:24 -0500

    Reading config information from a YAML file

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	go.mongodb.org/mongo-driver v1.4.4
+	gopkg.in/yaml.v2 v2.2.8
 )
--- pkg/repo/connect.go
+++ pkg/repo/connect.go
@@ -3,18 +3,20 @@ package repo
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/yaml.v2"
 )
 
 //GetClient gets a mongo client
 func GetClient() *mongo.Client {
-	fmt.Println(os.Getenv("MONGOTESTURI"))
 
-	clientOptions := options.Client().ApplyURI("mongodb+srv://mongouser:hpkns372@cluster0.dfch4.mongodb.net/test")
+	c, err := readConf()
+
+	clientOptions := options.Client().ApplyURI(c.Conf.Atlas)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		log.Fatal("Bad URI: ", err)
@@ -24,4 +26,26 @@ func GetClient() *mongo.Client {
 		log.Fatal(err)
 	}
 	return client
+}
+
+// NOTE: Fields in private structure MUST BE EXPORTED; upper case field names required here
+type myData struct {
+	Conf struct {
+		Atlas string `yaml:"atlas"`
+	}
+}
+
+func readConf() (*myData, error) {
+	filename := "config.yaml"
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := &myData{}
+	err = yaml.Unmarshal(buf, c) // Careful about structure name mapping
+	if err != nil {
+		return nil, fmt.Errorf("in file %q: %v", filename, err)
+	}
+
+	return c, nil
 }
